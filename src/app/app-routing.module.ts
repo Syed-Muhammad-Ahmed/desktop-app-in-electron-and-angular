0842e781fc8908c641f7eb501e126f54bc15c379
Syed Muhammad Ahmed <m.ahmed@commerciotech.com>
2023-04-17 13:50:15 +0500

    initial commit on home screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: "admin",
+  //   loadChildren: () =>
+  //     import("./admin/admin.module").then((m) => m.AdminModule),
+  //   canActivate: [AuthguardGuard],
+  // },
+  // {
+  //   path: "admin/login",
+  //   component: LoginComponent,
+  //   data: {
+  //     title: "Login",
+  //   },
+  // },
+
+  // by default, it will load client routes with base layout
+  { path: '', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
-  providers: [],
+  providers: [HomeComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
